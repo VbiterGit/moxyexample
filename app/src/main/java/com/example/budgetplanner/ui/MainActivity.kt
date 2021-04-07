b525e91df34acc968533d11be7e7cadfb7050bb1
Vladimir <Dudnik@magnit.ru>
2021-04-07 08:28:04 +0300

    Add AppBar

--- app/src/main/java/com/example/budgetplanner/ui/MainActivity.kt
+++ app/src/main/java/com/example/budgetplanner/ui/MainActivity.kt
@@ -2,11 +2,29 @@ package com.example.budgetplanner.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupWithNavController
 import com.example.budgetplanner.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = findNavController(R.id.nav_host_fragment)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        collapsing_toolbar_layout.setupWithNavController(toolbar, navController, AppBarConfiguration(navController.graph))
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
