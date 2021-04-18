d3410c999cc52678b23c43797fc077617f92a124
Vladimir <Dudnik@magnit.ru>
2021-04-18 15:13:08 +0300

    Add DrawerLayout for navigation

--- app/src/main/java/com/example/budgetplanner/ui/MainActivity.kt
+++ app/src/main/java/com/example/budgetplanner/ui/MainActivity.kt
@@ -2,20 +2,23 @@ package com.example.budgetplanner.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.navigation.ActivityNavigator
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
+import androidx.navigation.ui.*
+import androidx.navigation.ui.NavigationUI.setupWithNavController
 import com.example.budgetplanner.R
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main_drawer.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
     private lateinit var appBarConfiguration: AppBarConfiguration
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    /**
+     * onCreate with Toolbar and BottomNavigationView
+     */
+/*    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
@@ -29,14 +32,38 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.setHomeButtonEnabled(true)
 
         setupActionBarWithNavController(navController, appBarConfiguration)
-//        nav_view.setupWithNavController(navController)
-    }
+    }*/
 
 //    override fun onOptionsItemSelected(item: MenuItem): Boolean {
 //        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
 //    }
 
+    /**
+     * onCreate with DrawerLayout
+     *
+     */
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_drawer)
+
+        navController = findNavController(R.id.nav_host_fragment_vp)
+        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+
+        setSupportActionBar(toolbar_vp)
+        supportActionBar?.title = "Start Fragment"
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeButtonEnabled(true)
+
+        setupWithNavController(nav_view, navController)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
+    }
+
 }
